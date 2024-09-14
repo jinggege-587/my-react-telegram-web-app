78ae5bcfbc9e686d36c6900af75f5bdd0416d218
jhy <jinghanyu@vbhledger.com>
2024-09-14 11:12:54 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: jhy
+ * @Date: 2024-09-14 10:07:55
+ * @LastEditTime: 2024-09-14 10:16:42
+ * @LastEditors: jhy
+ * @Description: 
+ * @FilePath: /geyi/my-react-telegram-web-app/vite.config.ts
+ */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  build: {
+    outDir: './docs'
+  },
+  base: './'
+})
