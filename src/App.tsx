59249218df11db5c9a849509dab2398ca4a32bdb
jhy <jinghanyu@vbhledger.com>
2024-09-14 18:42:56 +0800

    5

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>{location.href}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
