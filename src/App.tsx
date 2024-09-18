933c899d8562c3ac7aea8af7374a572c02ef6010
111hunter <1621209357@qq.com>
2024-09-18 18:57:43 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
 // import bot from './TutorialBot'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [data, setData] = useState();
 
-  const getData = ()=>{
-    fetch('https://api.telegram.org/bot7498712718:AAGQ0ncMXgJt2hHBn_rpB90a3xDLjWlh4fQ/getMe').then((res)=>{
-      if(res.status === 200){
+  const getData = () => {
+    fetch(
+      "https://api.telegram.org/bot7498712718:AAGQ0ncMXgJt2hHBn_rpB90a3xDLjWlh4fQ/getMe"
+    ).then((res) => {
+      if (res.status === 200) {
         //json是返回的response提供的一个方法,会把返回的json字符串反序列化成对象,也被包装成一个Promise了
-        res.json().then((json)=>{
-          console.log('res: ', json)
+        res.json().then((json) => {
+          console.log("res: ", json);
           setData(json);
-        })
-      }else{
-        return {}
+        });
+      } else {
+        return {};
       }
     });
-
-  }
+  };
 
   // bot.start();
   return (
@@ -37,24 +38,28 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>{location.href}</p>
+      {/* <p>{location.href}</p> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
       <p>111</p>
-      <button onClick={()=>getData()}>getData</button>
+      <button onClick={() => getData()}>getData</button>
       <p>111</p>
-        {/* Here we add our button with alert callback */}
+      {/* Here we add our button with alert callback */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
+        <button
+          onClick={() =>
+            WebApp.showAlert(`Hello World! Current count is ${count}`)
+          }
+        >
+          Show Alert
         </button>
       </div>
       {JSON.stringify(data)}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@
  * @Date: 2024-09-14 10:07:55
  * @LastEditTime: 2024-09-14 10:16:42
  * @LastEditors: jhy
- * @Description: 
+ * @Description:
  * @FilePath: /geyi/my-react-telegram-web-app/vite.config.ts
  */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   build: {
-    outDir: './docs'
+    outDir: "./docs",
   },
-  base: './'
-})
+  base: "./",
+});
